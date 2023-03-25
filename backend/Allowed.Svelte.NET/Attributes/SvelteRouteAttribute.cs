91d88b59a6e8575071b11bb48f21d7048a2a1ba0
VodemSharp <vadumfedor@gmail.com>
2023-03-25 20:18:22 +0200

    Remove warning.

--- backend/Allowed.Svelte.NET/Attributes/SvelteRouteAttribute.cs
+++ backend/Allowed.Svelte.NET/Attributes/SvelteRouteAttribute.cs
@@ -5,7 +5,7 @@ namespace Allowed.Svelte.NET.Attributes;
 [AttributeUsage(AttributeTargets.Method, Inherited = false)]
 public class SvelteRouteAttribute : HttpGetAttribute
 {
-    public string Page { get; set; }
+    public string Page { get; set; } = null!;
     public string? Layout { get; set; }
     
     public SvelteRouteAttribute(string template = "")
